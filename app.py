c956581ccef8836456cb39aba539daa99667c5a4
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-11-14 23:12:20 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+import openai
+
+app = Flask(__name__)
+
+# Replace 'YOUR_OPENAI_API_KEY' with your actual OpenAI API key
+openai.api_key = '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'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate_response', methods=['POST'])
+def generate_response():
+    input_text = request.form['inputText']
+    instruction = request.form['instruction']
+    model = request.form['model']
+
+    # Make a request to OpenAI API
+    response = openai.Completion.create(
+        engine=model,
+        prompt=f"{instruction}\n\n{input_text}",
+        max_tokens=100  # You can customize other parameters as needed
+    )
+
+    # Get the generated text
+    generated_text = response['choices'][0]['text']
+
+    return jsonify({'generatedText': generated_text})
+
+if __name__ == '__main__':
+    app.run(debug=True)
